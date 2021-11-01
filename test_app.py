5b55b8c5ca0991e0a078b32265909d303ec1355f
Naser Al Madi <nsalmadi@colby.edu>
2021-11-01 17:50:40 -0400

    fixing website path

--- test_app.py
+++ test_app.py
@@ -2,12 +2,15 @@ from app import *
 import requests
 
 
+website_path = "https://salty-river-43291.herokuapp.com/"
+#website_path = "http://127.0.0.1:5000/"
+
 def test_about():
 	assert about() == "About us: Naser and the cool kids from CS321"
 
 
 def test_add():
-	url = 'http://127.0.0.1:5000/add'
+	url = website_path + '/add'
 	myobj = {'visitor': 'New person'}
 
 	webpage = requests.post(url, data = myobj)
